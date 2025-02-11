a8db6cb007c9f547628cc931446a64943aa1e5db
Jhansy Sreenivas Manchuri <sreenivasmj123@gmail.com>
2025-02-11 12:35:36 +0530

    Day 9

---
+++ SchedulingTasksSpringBoot/src/main/java/com/example/demo/SchedulingTasksApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+
+public class SchedulingTasksApplication {
+
+	public SchedulingTasksApplication() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ SchedulingTasksSpringBoot/src/main/java/com/example/demo/SchedulingTasksSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingTasksSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingTasksSpringBootApplication.class, args);
+	}
+
+}
